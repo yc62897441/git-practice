a095addc6a2be6470f12c66fce6b937d54f23337
yc62897441 <yc62897441@gmail.com>
2022-05-12 09:21:36 +0800

    project init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+const PORT = 3000
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(PORT, () => {
+  console.log(`Express router is running on http://localhost:${PORT}`)
+})
