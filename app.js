70c7aaa4b1e34669aa79d2dab9ed37958a5f8f97
yc62897441 <yc62897441@gmail.com>
2022-05-12 09:56:49 +0800

    branch3 insert line 20

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.get('/branch1', (req, res) => {
 })
 
 app.get('/branch2', (req, res) => {
+  console.log('branch3 update 2022/5/12 09:55 insert this to line 20')
   res.render('branch2')
 })
 
