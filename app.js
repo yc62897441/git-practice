126fe71be7e96ca4979c094e07f273594a40f83d
yc62897441 <yc62897441@gmail.com>
2022-05-12 09:35:19 +0800

    add: branch1 router and view

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/branch1', (req, res) => {
+  res.render('branch1')
+})
+
 app.listen(PORT, () => {
   console.log(`Express router is running on http://localhost:${PORT}`)
 })
